6fca1fa504392b5f31e063bfc7e8538262fb8e57
Arman <a.shargynov@enpf.kz>
2025-07-24 11:47:20 +0500

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="flex justify-center h-screen bg-gray-100">
+  <h1 class="text-3xl font-bold text-blue-500">
+    Привет, Tailwind CSS!!!
+  </h1>
+</div>
+<Button label="Click Me" />
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
