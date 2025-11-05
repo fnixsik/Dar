d7720a602c10887a74c749f6c388b160a73e467e
Arman <a.shargynov@enpf.kz>
2025-11-05 16:29:59 +0500

    удалил лишние файлы

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { API_BASE_URL } from "@/shared/service/api"
 
 export const BaseApi = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: API_BASE_URL,
     withCredentials: true,
 });
--- src/shared/api/accessServiceApi.ts
+++
@@ -1,7 +0,0 @@
-import { createApiClient } from '../service/createApiClient'
-import { API_BASE_URL } from '../service/api'
-
-export const accessServiceApi = createApiClient({
-    baseURL: API_BASE_URL,
-
-})
